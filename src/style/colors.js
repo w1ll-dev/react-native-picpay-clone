e069297bbe63ddca3b80348139f60cb6d5b71775
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-21 21:28:47 -0300

    add colors file

---
+++ src/style/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  iconActive: "#fff",
+  iconInnactive: "#92929c",
+
+  payButtonActiveGradient: ["#00fc6c", "#00ac4a"],
+  payButtonInactiveGradient: ["#fff", "#ccc"],
+
+  white: "#fff",
+  black: "#131418",
+  grey: "#323232"
+};
