0a6e6acae5f2679893296330181bcf57cb231f6d
will-dev <w1llmarcio.dev@hotmail.com>
2020-09-24 19:11:49 -0300

    add suggestions component

--- src/components/Sugestions/style.js
+++ src/components/Sugestions/style.js
@@ -3,7 +3,26 @@ import { colors } from '../../style/colors';
 
 export const ScrollContainer = styled.ScrollView.attrs(() => ({
   horizontal: true,
+  contentContainerStyle: {
+    alignItems: "center",
+    paddingLeft: 16
+  },
 }))`
   background: ${colors.lightGrey};
   height: 130px;
+`;
+
+export const Option = styled.TouchableOpacity`
+  margin-right: 16px;
+  align-items: center;
+`;
+
+export const Img = styled.Image`
+`;
+
+export const Label = styled.Text`
+  color: ${colors.white};
+  font-size: 14px;
+  margin-top: 8px;
+  font-weight: bold;
 `;
